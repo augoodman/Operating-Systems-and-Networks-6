6adf91caba9293260c851d2f82cb0814642745bf
augoodma <augoodma@asu.edu>
2020-09-10 13:15:22 -0400

    first commit

---
+++ GoodmanFilters.c
@@ -0,0 +1,4 @@
+//
+// Created by andrew on 9/10/20.
+//
+
---
+++ cmake-build-debug/CMakeFiles/Module6.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module6.dir/BaseFilters.c.o"
+  "CMakeFiles/Module6.dir/GoodmanFilters.c.o"
+  "Module6"
+  "Module6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Module6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
